1cf5154ac0649ca6286e2d4cde9792af11635d56
quy2002 <trandinhquyzz2002@gmail.com>
2021-05-26 22:37:15 +0700

    connit vao day h toi

---
+++ T2009M/lap3/Cart.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T2009M
+{
+    public class Cart
+    {
+        private int id;
+        private string customer;
+        private double grandTotal;
+        private List<Product> productList = new List<Product>();
+        private string city;
+        private string country;
+
+        public Cart()
+        {
+
+        }
+
+        public Cart(int id, string customer, double grandTotal, string city, string country)
+        {
+            this.id = id;
+            this.customer = customer;
+            this.grandTotal = grandTotal;
+            this.city = city;
+            this.country = country;
+        }
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Customer
+        {
+            get => customer;
+            set => customer = value;
+        }
+
+        public double GrandTotal
+        {
+            get => grandTotal;
+            set => grandTotal = value;
+        }
+
+        public Product this[int index]
+        {
+            get => productList[index];
+            set => productList[index] = value;
+        }
+
+        public string City
+        {
+            get => city;
+            set => city = value;
+        }
+
+        public string Country
+        {
+            get => country;
+            set => country = value;
+        }
+
+        public void AddNewProduct(Product prd)
+        {
+            productList.Add(prd);
+        }
+
+        public void RemoveProduct(Product prd)
+        {
+            productList.Remove(prd);
+        }
+
+        public void GetProductInCart()
+        {
+            foreach (Product prod in productList)
+            {
+                Console.WriteLine("\t ");
+                prod.GetInfo();
+            }
+        }
+
+        public double CalcGrandTotal()
+        {
+            if (City == "HN" || City == "HCM")
+            {
+                GrandTotal = GrandTotal + GrandTotal * 1 / 100;
+            }
+            else if (Country == "Vietnam")
+            {
+                GrandTotal = GrandTotal + GrandTotal * 2 / 100;
+            }
+            else
+            {
+                GrandTotal = GrandTotal + GrandTotal * 5 / 100;
+            }
+
+            return GrandTotal;
+        }
+
+
+    }
+}
---
+++ T2009M/lap3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T2009M
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Diamond dim = new Diamond(1, "Diamond Black", 10000, 1, "diamond.png", "This is a black diamond", 20);
+            Fashion fs1 = new Fashion(2, "T-Shirt", 2000, 2, "t-shirt.jpg", "This is T-shirt", "red", "XL");
+            Fashion fs2 = new Fashion(3, "Adidas Ultraboost", 5000, 1, "ultraboot.png", "This is Ultraboost", "black", "44");
+            Cart ca = new Cart(1, "Dang Jinner", 14000, "Ninh Binh", "Vietnam");
+            ca.AddNewProduct(dim);
+            ca.AddNewProduct(fs1);
+            ca.AddNewProduct(fs2);
+            ca.RemoveProduct(fs2);
+            Console.WriteLine("ID khach hang: " + ca.Id);
+            Console.WriteLine("Ten khach hang: " + ca.Customer);
+            Console.WriteLine("Thanh pho: " + ca.City);
+            Console.WriteLine("Quoc gia: " + ca.Country);
+            Console.WriteLine("Gio hang: ");
+            Console.WriteLine("---------------");
+            ca.GetProductInCart();
+            Console.WriteLine("---------------");
+            Console.WriteLine("Tong tien la: " + ca.CalcGrandTotal());
+
+        }
+    }
+}
