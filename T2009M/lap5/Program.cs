c235b4bf44a53303b5d04ad31602493854f130ae
quy2002 <trandinhquyzz2002@gmail.com>
2021-06-07 19:31:05 +0700

    up code

---
+++ T2009M/lap5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace T2009M.lab.s5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread boom = new Thread(Boom);
+            boom.Start();
+
+        }
+
+        public static void Boom()
+        {
+            int min = 10;
+            int sec = 0;
+            while (min >= 0)
+            {
+                if (sec == 0)
+                {
+                    sec = 60;
+                    min--;
+                }
+                sec--;
+                if (min < 10)
+                {
+                    if (sec < 10)
+                    {
+                        Console.WriteLine("0" + min + ":" + "0" + sec);
+                    }
+                    else
+                    {
+                        Console.WriteLine("0" + min + ":" + sec);
+                    }
+                }
+
+                if (min == 0 && sec == 0) Console.WriteLine(" ***** Bùmmmmmmm *****");
+
+                try
+                {
+                    Thread.Sleep(10);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+
+            }
+
+        }
+    }
+
+
+}
